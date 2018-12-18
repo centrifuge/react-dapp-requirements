07acbc6ba0374ecff871bd9eb87923f93e397ac2
Napat <elecwebmaker@gmail.com>
2018-12-18 19:34:32 +0700

    format code with prettier

--- src/utils.js
+++ src/utils.js
@@ -1,29 +1,25 @@
 export const promisify = (inner) => {
-    return new Promise((resolve, reject) => {
-        inner((error, result) => {
-            if (error) {
-                reject(error)
-            }
-            resolve(result);
-        });
-    })
+  return new Promise((resolve, reject) => {
+    inner((error, result) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    });
+  });
 };
 
 export const promiseWithTimeout = (millis, promise) => {
-    const timeout = new Promise((resolve, reject) =>
-        setTimeout(
-            () => reject(`promise.timeout`),
-            millis));
-    return Promise.race([
-        promise,
-        timeout
-    ]);
+  const timeout = new Promise((resolve, reject) =>
+    setTimeout(() => reject(`promise.timeout`), millis)
+  );
+  return Promise.race([promise, timeout]);
 };
 
 export const centeredStyle = {
-    "height": "100vh",
-    "display": "flex",
-    "flexDirection": "column",
-    "alignItems": "center",
-    "justifyContent": "center",
-}
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+};
